caf1babbba6ba5ce94455b464e5ba75db04eba06
ruann08072008 <silva.ruann@escola.pr.gov.br>
2026-04-15 22:07:16 -0300

    Add files via upload

---
+++ tipos.js
@@ -0,0 +1,32 @@
+// boolean 
+
+let ativo = true;
+
+console.log(typeof ativo);
+console.log(ativo);
+
+// number
+let idade = 25;
+let pi = 3.14;
+console.log(typeof idade);
+console.log(typeof pi);
+
+//string e templante literals
+let nome = "maria";
+let mensagem = `ola, ${nome}`;
+console.log(mensagem);
+
+//undefined
+let valorindefinido;
+console.log(typeof valorindefinido);
+console.log(valorindefinido);
+
+// null
+let valornulo = null;
+console.log(typeof valornulo);
+console.log(valornulo);
+
+//bigint
+let numerogrande = 9007199254740991n;
+console.log(typeof nemerogrande);
+console.log(numerogrande);
